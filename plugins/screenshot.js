cfa32e3e9fa7f98912b7575d1992760a0337c7c5
jayvon33 <stephenyayravondee@gmail.com>
2025-12-19 21:04:14 +0000

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "TREND X";
+        let pack = Config.STICKER_NAME || "LIMKID X";
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
