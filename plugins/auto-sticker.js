bfd4237f936b01984d40375a3d6305983be74dc4
jayvon33 <stephenyayravondee@gmail.com>
2025-12-19 18:51:06 +0000

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -17,7 +17,7 @@ cmd({
             from,
             {
               sticker: { url: data[keyword] },
-              package: 'TREND-X'
+              package: 'LIMKID-X'
             },
             { quoted: mek }
           );
